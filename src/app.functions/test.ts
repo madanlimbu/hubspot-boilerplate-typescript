39320f4df3214418fd42d35505558af45e31e4e9
Madan Limbu <madanlimbu.dev@gmail.com>
2021-06-11 01:25:26 +0100

    Add basic support for typescript with serverless function on webpack config

---
+++ src/app.functions/test.ts
@@ -0,0 +1,22 @@
+interface RequestContext {
+    params: any;
+    body: any;
+    headers: {
+        method: 'GET' | 'POST' | 'PATCH';
+    };
+    contact?: {
+        vid: number;
+    };
+}
+
+type SendResponse = (arg: any) => void;
+
+export const main = (context: RequestContext, sendResponse: SendResponse) => {
+    // $HUBSPOT_HOST/_hcms/api/test to test.
+    sendResponse({
+        body: {
+            hubspot: 'Serverless function with typescript'
+        },
+        statusCode: 200
+    });
+};
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+
+// Uncomment if you have full hubspot setup locally to auto-sync local changes.
+// const THEME_NAME = 'hubspot-boilerplate-typescript';
+// const HubSpotAutoUploadPlugin = require('@hubspot/webpack-cms-plugins/HubSpotAutoUploadPlugin')
+const CopyPlugin = require('copy-webpack-plugin');
+const path = require("path");
+
+const serverlessFunctionConfig = ({ account, autoupload }) => ({
+    target: 'node',
+    entry: {
+        test: './src/app.functions/test.ts'
+    },
+    module: {
+        rules: [{
+            test: /\.ts?$/,
+            include: [
+                path.resolve(__dirname, 'src/app.functions'),
+            ],
+            loader: 'babel-loader',
+        }, ],
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "app.functions/[name].js",
+        library: {
+            type: 'umd'
+        }
+    },
+    plugins: [
+        // new HubSpotAutoUploadPlugin({
+        //     autoupload,
+        //     account,
+        //     src: 'dist',
+        //     dest: THEME_NAME,
+        // }),
+        new CopyPlugin({
+            patterns: [
+                { from: "src/app.functions/serverless.json", to: "app.functions/serverless.json" },
+            ]
+        }),
+    ],
+});
+
+module.exports = [serverlessFunctionConfig];
